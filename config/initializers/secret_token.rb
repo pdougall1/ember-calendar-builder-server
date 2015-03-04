4d5bff099da25e5046988cd523dd12ec8218bb36
Patrick <pdougall1@gmail.com>
2015-03-04 02:43:17 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmberCalendarBuilderServer::Application.config.secret_key_base = '1b6daea2e8ad0ba3effe5855ce4eb864b09860ad38504046275b480751e1ee1c860891a61a9aa9e2a8bbe4fcffa6c91ca9f0f4ae0d63ac8b37173eda2450aaba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EmberCalendarBuilderServer::Application.config.session_store :cookie_store, key: '_ember-calendar-builder-server_session'
